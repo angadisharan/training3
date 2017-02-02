6c959ecbbd08790361306f82f9fc857f1e57071f
Sharan Angadi <sharan@mathrusoft.com>
2017-02-02 12:06:56 +0530

    android one

---
+++ Andorid_TestOne/app/src/main/java/com/mathrusoft/testone/MyLauncherActivity.java
@@ -0,0 +1,22 @@
+package com.mathrusoft.testone;
+
+import android.app.Activity;
+import android.os.Bundle;
+
+/**
+ * Created by sharanangadi on 02/02/17.
+ */
+
+public class MyLauncherActivity extends Activity {
+
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+
+        setContentView(R.layout.activity_launcher);
+
+
+
+    }
+}
