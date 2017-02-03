1ac8d0daa819f9239a445cc9df63e0cdb4f1e0e8
Sharan Angadi <sharan@mathrusoft.com>
2017-02-03 12:13:27 +0530

    Collections model

--- CollectionsDemo/src/MainCollections.java
+++ CollectionsDemo/src/MainCollections.java
@@ -1,4 +1,6 @@
 import java.util.ArrayList;
+import java.util.Collections;
+import java.util.Comparator;
 import java.util.HashMap;
 import java.util.LinkedList;
 import java.util.List;
@@ -7,37 +9,69 @@ import java.util.TreeMap;
 
 import javax.swing.text.html.HTMLDocument.Iterator;
 
-
 public class MainCollections {
 
 	public static void main(String[] args) {
+
+		List<ModelStudent> modelStudentList = new ArrayList<ModelStudent>();
+
+		Map<String, ModelStudent> map = new HashMap<String, ModelStudent>();
 		
 		
-		List<String> myList = new LinkedList(); 
+		
+		
+		ModelStudent student = new ModelStudent();
+		student.setName("Mahesh");
+		student.setParentName("Suresh");
+		modelStudentList.add(student);
+		
+		map.put("420", student);
 
-		myList.add("First string");
-		myList.add("Second string");
-		myList.add("Third string");
-		myList.add("Fourth string");
+		student = new ModelStudent();
+		student.setName("Sachin");
+		student.setParentName("Anand");
+		modelStudentList.add(student);
 
-		for (int i = 0; i < myList.size(); i++) {
-			System.out.println(" string at position  " + i + " " + myList.get(i));	
+		Comparator<ModelStudent> comparator = new Comparator<ModelStudent>() {
+
+			@Override
+			public int compare(ModelStudent std1, ModelStudent std2) {
+				return std1.getParentName().compareTo(std2.getParentName());
+			}
+		};
+
+//		Collections.sort(modelStudentList, comparator);
+		for (int i = 0; i < modelStudentList.size(); i++) {
+			System.out.println(" string at position  " + i + " "
+					+" student name " +  modelStudentList.get(i).getName() 
+					+ " parent name " + modelStudentList.get(i).getParentName());
 		}
-		
-		System.out.println("============================");
-		
-		Map<String, String> myMap = new HashMap<String, String>(); 
 
-		myMap.put("one", "One value");
-		myMap.put("two", "Two value");
-		myMap.put("three", "Three value");
-		myMap.put("four", "Four value");
-		myMap.put("five", "Five value");
-		
-		for (String key : myMap.keySet()) {
-			System.out.println(" string at key:  " + key + " " + myMap.get(key));	
-		}		
-		
+		// List<String> myList = new LinkedList();
+		//
+		// myList.add("First string");
+		// myList.add("Second string");
+		// myList.add("Third string");
+		// myList.add("Fourth string");
+		//
+		// for (int i = 0; i < myList.size(); i++) {
+		// System.out.println(" string at position  " + i + " " +
+		// myList.get(i));
+		// }
+		//
+		// System.out.println("============================");
+		//
+		// Map<String, String> myMap = new HashMap<String, String>();
+		//
+		// myMap.put("one", "One value");
+		// myMap.put("two", "Two value");
+		// myMap.put("three", "Three value");
+		// myMap.put("four", "Four value");
+		// myMap.put("five", "Five value");
+		//
+		// for (String key : myMap.keySet()) {
+		// System.out.println(" string at key:  " + key + " " + myMap.get(key));
+		// }
+
 	}
-
 }
---
+++ CollectionsDemo/src/ModelStudent.java
@@ -0,0 +1,20 @@
+
+public class ModelStudent {
+	private String name;
+	private String parentName;
+
+	public String getName() {
+		return name;
+	}
+	public void setName(String name) {
+		this.name = name;
+	}
+	public String getParentName() {
+		return parentName;
+	}
+	public void setParentName(String parentName) {
+		this.parentName = parentName;
+	}
+	
+	
+}
