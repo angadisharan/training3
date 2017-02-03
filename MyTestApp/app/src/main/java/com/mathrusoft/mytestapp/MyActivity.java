f5e2bbcfffbd9e6734d78217ab9055297832a7cb
Sharan Angadi <sharan@mathrusoft.com>
2017-02-03 12:13:42 +0530

    Android test app

---
+++ MyTestApp/app/src/main/java/com/mathrusoft/mytestapp/MyActivity.java
@@ -0,0 +1,18 @@
+package com.mathrusoft.mytestapp;
+
+import android.app.Activity;
+import android.os.Bundle;
+
+/**
+ * Created by sharanangadi on 03/02/17.
+ */
+
+public class MyActivity extends Activity {
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+
+        setContentView(R.layout.my_activity);
+    }
+}
