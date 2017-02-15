47b288a091f5e3ba50cb4d1b8fed35672355e4a2
Sharan Angadi <sharan@mathrusoft.com>
2017-02-15 12:17:49 +0530

    Nav bar

---
+++ DemoNavigationActivity/app/src/main/java/com/mathrusoft/demonavigationactivity/ActivityMain.java
@@ -0,0 +1,134 @@
+package com.mathrusoft.demonavigationactivity;
+
+import android.content.Context;
+import android.os.Bundle;
+import android.support.design.widget.FloatingActionButton;
+import android.support.design.widget.NavigationView;
+import android.support.v4.app.Fragment;
+import android.support.v4.app.FragmentManager;
+import android.support.v4.app.FragmentTransaction;
+import android.support.v4.view.GravityCompat;
+import android.support.v4.widget.DrawerLayout;
+import android.support.v7.app.ActionBarDrawerToggle;
+import android.support.v7.app.AppCompatActivity;
+import android.support.v7.widget.Toolbar;
+import android.view.Menu;
+import android.view.MenuItem;
+import android.view.View;
+import android.widget.Toast;
+
+import com.mathrusoft.demonavigationactivity.fragment.FragmentCamera;
+import com.mathrusoft.demonavigationactivity.fragment.FragmentGallery;
+import com.mathrusoft.demonavigationactivity.fragment.FragmentSettings;
+import com.mathrusoft.demonavigationactivity.fragment.FragmentSlideShow;
+
+public class ActivityMain extends AppCompatActivity {
+
+    Context mContex;
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_main);
+
+        mContex = this.getApplicationContext();
+
+        Toolbar toolbar = (Toolbar) findViewById(R.id.toolbar);
+        setSupportActionBar(toolbar);
+
+        FloatingActionButton fab = (FloatingActionButton) findViewById(R.id.fab);
+        fab.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View view) {
+                Toast.makeText(ActivityMain.this, "Fab clicked ", Toast.LENGTH_SHORT).show();
+            }
+        });
+
+        DrawerLayout drawer = (DrawerLayout) findViewById(R.id.drawer_layout);
+        ActionBarDrawerToggle toggle = new ActionBarDrawerToggle(
+                this, drawer, toolbar, R.string.navigation_drawer_open, R.string.navigation_drawer_close);
+        drawer.setDrawerListener(toggle);
+        toggle.syncState();
+
+        NavigationView navigationView = (NavigationView) findViewById(R.id.nav_view);
+        navigationView.setNavigationItemSelectedListener(mNavigationItemSelectedListener);
+    }
+
+    @Override
+    public void onBackPressed() {
+        DrawerLayout drawer = (DrawerLayout) findViewById(R.id.drawer_layout);
+        if (drawer.isDrawerOpen(GravityCompat.START)) {
+            drawer.closeDrawer(GravityCompat.START);
+        } else {
+            super.onBackPressed();
+        }
+    }
+
+    @Override
+    public boolean onCreateOptionsMenu(Menu menu) {
+        // Inflate the menu; this adds items to the action bar if it is present.
+        getMenuInflater().inflate(R.menu.activity_main, menu);
+        return true;
+    }
+
+    @Override
+    public boolean onOptionsItemSelected(MenuItem item) {
+        // Handle action bar item clicks here. The action bar will
+        // automatically handle clicks on the Home/Up button, so long
+        // as you specify a parent activity in AndroidManifest.xml.
+        int id = item.getItemId();
+
+        //noinspection SimplifiableIfStatement
+        if (id == R.id.action_settings) {
+            return true;
+        }
+
+        return super.onOptionsItemSelected(item);
+    }
+
+
+    NavigationView.OnNavigationItemSelectedListener mNavigationItemSelectedListener = new NavigationView.OnNavigationItemSelectedListener() {
+        @SuppressWarnings("StatementWithEmptyBody")
+        @Override
+        public boolean onNavigationItemSelected(MenuItem item) {
+            // Handle navigation view item clicks here.
+            int id = item.getItemId();
+
+            if (id == R.id.nav_camera) {
+                changeFragment(new FragmentCamera());
+                Toast.makeText(mContex, "Camera clicked", Toast.LENGTH_SHORT).show();
+            } else if (id == R.id.nav_gallery) {
+                changeFragment(new FragmentGallery());
+                Toast.makeText(mContex, "nav_gallery clicked", Toast.LENGTH_SHORT).show();
+            } else if (id == R.id.nav_slideshow) {
+                changeFragment(new FragmentSlideShow());
+                Toast.makeText(mContex, "nav_slideshow clicked", Toast.LENGTH_SHORT).show();
+            } else if (id == R.id.nav_manage) {
+                changeFragment(new FragmentSettings());
+                Toast.makeText(mContex, "nav_manage clicked", Toast.LENGTH_SHORT).show();
+            } else if (id == R.id.nav_share) {
+
+
+                Toast.makeText(mContex, "nav_share clicked", Toast.LENGTH_SHORT).show();
+            } else if (id == R.id.nav_send) {
+
+
+                Toast.makeText(mContex, "nav_send clicked", Toast.LENGTH_SHORT).show();
+            }
+
+            DrawerLayout drawer = (DrawerLayout) findViewById(R.id.drawer_layout);
+            drawer.closeDrawer(GravityCompat.START);
+            return true;
+        }
+    };
+
+
+    private void changeFragment(Fragment fragment) {
+        FragmentManager fragmentManager = getSupportFragmentManager();
+        FragmentTransaction fragmentTransaction = fragmentManager.beginTransaction();
+        fragmentTransaction.replace(R.id.fragment_holder, fragment);
+        fragmentTransaction.commit();
+    }
+
+
+}
---
+++ DemoNavigationActivity/app/src/main/java/com/mathrusoft/demonavigationactivity/fragment/FragmentSlideShow.java
@@ -0,0 +1,24 @@
+package com.mathrusoft.demonavigationactivity.fragment;
+
+import android.os.Bundle;
+import android.support.annotation.Nullable;
+import android.support.v4.app.Fragment;
+import android.view.LayoutInflater;
+import android.view.View;
+import android.view.ViewGroup;
+
+import com.mathrusoft.demonavigationactivity.R;
+
+/**
+ * Created by sharanangadi on 15/02/17.
+ */
+
+public class FragmentSlideShow extends Fragment {
+
+    @Nullable
+    @Override
+    public View onCreateView(LayoutInflater inflater, @Nullable ViewGroup container, @Nullable Bundle savedInstanceState) {
+        View view = inflater.inflate(R.layout.fragment_slideshow, null);
+        return  view;
+    }
+}
